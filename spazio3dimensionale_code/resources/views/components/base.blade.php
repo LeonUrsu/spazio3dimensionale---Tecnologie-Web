893a16c4578adb58ccaac49eddf435bcf4a4a7d5
leonursu <leonursu1998@gmail.com>
2026-01-08 18:13:00 +0100

    lavoro sul css front-end

--- spazio3dimensionale_code/resources/views/components/base.blade.php
+++ spazio3dimensionale_code/resources/views/components/base.blade.php
@@ -7,13 +7,52 @@
 </head>
 
 <body>
-    <header>
-    
+    <header class="container">
+        <nav class="menu-box">
+            <div class="logo-container">
+                <a href="/">
+                    <img src="{{ asset('images/logo_spazio3Dimensionale.png') }}" alt="Logo Spazio3D" class="site-logo">
+                </a>
+            </div>
+            <form action="{{route('centro.lista')}}" method=GET>
+                @csrf
+                <button class="menu-button" type="submit">Centri Disponibili</button>
+            </form>
+
+            <form action="{{route('prodotto.lista')}}" method=GET>
+                @csrf
+                <button type="submit">Catalogo Prodotti</button>
+            </form>
+
+            @can('isAdmin')
+            <form action="{{route('tecnico.centro.lista')}}" method=GET>
+                <button type="submit">Vedi Tecnici Centri</button>
+            </form>
+            <form action="{{route('tecnico.azienda.lista')}}" method=GET>
+                <button type="submit">Vedi Tecnici Azienda</button>
+            </form>
+            @endcan
+
+            @guest
+            <form action="{{route('login')}}" method=GET>
+                @csrf
+                <button type="submit">Login</button>
+            </form>
+            @endguest
+
+            @auth
+            <form action="{{route('logout')}}" method=POST onsubmit="return confirm('Sei sicuro di voler procedere con il logout?')">
+                @csrf
+                <button type="submit">Logout</button>
+            </form>
+            @endauth
+
+        </nav>
     </header>
-    <main>
+    <main class="container">
         {{ $slot }}
     </main>
-    <footer>
+    <footer class="container">
         <p> Chiamaci a: {{ config('azienda.telefono') }} </p>
         <p> Scrivici a: {{ config('azienda.email') }} </p>
         <p> Vineni a trovarci in: {{ config('azienda.indirizzo') }} </p>
--- spazio3dimensionale_code/resources/views/components/home.blade.php
+++ spazio3dimensionale_code/resources/views/components/home.blade.php
@@ -1,33 +1,8 @@
-    <div class="nav-links">
+    <div class="home-box">
         <div>{{ Breadcrumbs::render() }}</div>
-        <div>
-            @guest
-            <form action="{{route('login')}}" method=GET>
-                @csrf
-                <button type="submit">Login</button>
-            </form>
-            @endguest
-            @auth
-            <form action="{{route('logout')}}" method=POST onsubmit="return confirm('Sei sicuro di voler procedere con il logout?')">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-            @endauth
-            <form action="{{route('centro.lista')}}" method=GET>
-                @csrf
-                <button type="submit">Centri Disponibili</button>
-            </form>
-            <form action="{{route('prodotto.lista')}}" method=GET>
-                @csrf
-                <button type="submit">Catalogo Prodotti</button>
-            </form>
-            @can('isAdmin')
-            <form action="{{route('tecnico.centro.lista')}}" method=GET>
-                <button type="submit">Vedi Tecnici Centri</button>
-            </form>
-            <form action="{{route('tecnico.azienda.lista')}}" method=GET>
-                <button type="submit">Vedi Tecnici Azienda</button>
-            </form>
-            @endcan
-        </div>
+            <div class="home-row">
+                <div class="home-element">
+                    <p>ELEMENTO MOSTRATO</p>
+                </div>
+            </div>
     </div>
